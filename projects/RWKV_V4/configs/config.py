6c693308bf0697ed74d1ab0ff99496247429cd3b
ChenQiaoLing <2398653527@qq.com>
2022-07-24 04:16:56 +0000

    update

--- projects/RWKV_V4/configs/config.py
+++ projects/RWKV_V4/configs/config.py
@@ -3,9 +3,10 @@ from omegaconf import OmegaConf
 from libai.config import get_config
 from libai.config import LazyCall
 from libai.tokenizer import GPT2Tokenizer
+import pdb
 
 # 配置 model
-from projects.RWKV_V4.modeling.model import GPT 
+from projects.RWKV_V4.modeling.model import GPT ,GPTConfig
 # 配置 dataloader `build_image_train_loader` 和 `build_image_test_loader` 是 LiBai 提供的用于创建图像数据的训练集和测试集 DataLoader 的两个函数
 from libai.data.build import build_nlp_test_loader, build_nlp_train_loader
 # 导入自定义的 dataset
@@ -21,19 +22,16 @@ graph = get_config("common/models/graph.py").graph
 
 
 # 配置model
-model = LazyCall(GPT)(cfg=model_cfg)
-# model.cfg.embedding_dropout_prob = 0.1
-# model.cfg.attention_dropout_prob = 0.1
-# model.cfg.num_attention_heads = 16
-# model.cfg.hidden_size = 384
-# model.cfg.ffn_hidden_size = 1536
-# model.cfg.num_layers = 6
+model = LazyCall(GPT(GPTConfig(6064, 1024, model_type='RWKV',
+                        n_layer=6, n_embd=512)))
+
+
 
 
 # 训练过程
 train = get_config("common/train.py").train
 train.input_placement_device = "cpu"
-train.dist.pipeline_num_layers = model.cfg.num_layers
+train.dist.pipeline_num_layers = 6
 
 
 # 获得一个 DataLoader 的配置对象
--- projects/RWKV_V4/modeling/model.py
+++ projects/RWKV_V4/modeling/model.py
@@ -1,66 +1,21 @@
-import logging
-
+import numpy as np
 import oneflow as flow
-from oneflow import nn
-
-from libai.layers import Linear
-from libai.models.gpt_model import GPTModel
+import oneflow.nn.functional as F
+from oneflow import einsum, nn
+import math, os
+import logging
+from libai.config import configurable
+from libai.layers import LayerNorm, Linear, LMLogits, ParallelCrossEntropyLoss, VocabEmbedding
 from libai.models.utils import init_method_normal
 from libai.utils import distributed as dist
+logger = logging.getLogger(__name__)
 
-logger = logging.getLogger("libai." + __name__)
-
-def RWKV_Init(module, config):  # fancy initialization of all lin & emb layer in the module
-    print('\n[--> first run, init model params (very slow for large models) <--]')
-    print('[so you shall only do it for 1 single GPU and save the checkpt and load it when using multiple GPU]\n')
-    for m in module.modules():
-        if not isinstance(m, (nn.Linear, nn.Embedding)):
-            continue
-        with torch.no_grad():
-            name = '[unknown weight]'
-            for name, parameter in module.named_parameters():  # find the name of the weight
-                if id(m.weight) == id(parameter):
-                    break
-
-            shape = m.weight.data.shape
-            gain = 1.0
-            scale = 1.0  # extra scale for gain
-
-            if isinstance(m, nn.Embedding):
-                gain = math.sqrt(max(shape[0], shape[1]))
-                if shape[0] == config.vocab_size and shape[1] == config.n_embd:  # token emb?
-                    scale = 1e-4
-                else:
-                    scale = 0
-
-            if isinstance(m, nn.Linear):
-                if m.bias is not None:
-                    m.bias.data.zero_()
-                if shape[0] > shape[1]:
-                    gain = math.sqrt(shape[0] / shape[1])
-                if shape[0] == config.vocab_size and shape[1] == config.n_embd:  # final projection?
-                    scale = 0.5
-
-            if hasattr(m, 'scale_init'):
-                scale = m.scale_init
-
-            # print(str(shape[0]).ljust(5), str(shape[1]).ljust(5), f'{round(scale,2):g}'.ljust(4), name)
-
-            gain *= scale
-            if scale == -999:
-                nn.init.eye_(m.weight)
-            elif gain == 0:
-                # zero init is great for some RWKV matrices
-                nn.init.zeros_(m.weight)
-            elif gain > 0:
-                nn.init.orthogonal_(m.weight, gain=gain)
-            else:
-                nn.init.normal_(m.weight, mean=0.0, std=-scale)
-
+RWKV_HEAD_QK_DIM = 256
+print(f'\nRWKV_HEAD_QK_DIM {RWKV_HEAD_QK_DIM}\n')
 
 class RWKV_TimeMix(nn.Module):
     def __init__(self, config, layer_id):
-
+       
         super().__init__()
         self.layer_id = layer_id
         self.ctx_len = config.ctx_len
@@ -68,36 +23,36 @@ class RWKV_TimeMix(nn.Module):
 
         attn_sz = config.n_embd
 
-        with torch.no_grad(): # fancy init
+        with flow.no_grad(): # fancy init
             ratio_0_to_1 = (layer_id / (config.n_layer - 1)) # 0 to 1
             ratio_1_to_almost0 = (1.0 - (layer_id / config.n_layer)) # 1 to ~0
-
+            
             # fancy time_decay
-            decay_speed = torch.ones(attn_sz)
+            decay_speed = flow.ones(attn_sz)
             for h in range(attn_sz):
                 decay_speed[h] = -5 + 8 * (h / (attn_sz-1)) ** (0.7 + 1.3 * ratio_0_to_1)
             self.time_decay = nn.Parameter(decay_speed)
             # print(layer_id, self.time_decay.flatten()[:3].cpu().numpy(), '...', self.time_decay.flatten()[-3:].cpu().numpy())
 
             # fancy time_first
-            zigzag = (torch.tensor([(i+1)%3 - 1 for i in range(attn_sz)]) * 0.5)
-            self.time_first = nn.Parameter(torch.ones(attn_sz) * math.log(0.3) + zigzag)
-
+            zigzag = (flow.tensor([(i+1)%3 - 1 for i in range(attn_sz)]) * 0.5)
+            self.time_first = nn.Parameter(flow.ones(attn_sz) * math.log(0.3) + zigzag)
+            
             # fancy time_mix
-            x = torch.ones(1, 1, config.n_embd)
+            x = flow.ones(1, 1, config.n_embd)
             for i in range(config.n_embd):
                 x[0, 0, i] = i / config.n_embd
-            self.time_mix_k = nn.Parameter(torch.pow(x, ratio_1_to_almost0))
-            self.time_mix_v = nn.Parameter(torch.pow(x, ratio_1_to_almost0) + 0.3 * ratio_0_to_1)
-            self.time_mix_r = nn.Parameter(torch.pow(x, 0.5 * ratio_1_to_almost0))
-
+            self.time_mix_k = nn.Parameter(flow.pow(x, ratio_1_to_almost0))
+            self.time_mix_v = nn.Parameter(flow.pow(x, ratio_1_to_almost0) + 0.3 * ratio_0_to_1)
+            self.time_mix_r = nn.Parameter(flow.pow(x, 0.5 * ratio_1_to_almost0))
+            
         self.time_shift = nn.ZeroPad2d((0, 0, 1, -1))
 
-        self.key = nn.Linear(config.n_embd, attn_sz, bias=False)
-        self.value = nn.Linear(config.n_embd, attn_sz, bias=False)
-        self.receptance = nn.Linear(config.n_embd, attn_sz, bias=False)
+        self.key = Linear(config.n_embd, attn_sz, bias=False)
+        self.value = Linear(config.n_embd, attn_sz, bias=False)
+        self.receptance = Linear(config.n_embd, attn_sz, bias=False)
 
-        self.output = nn.Linear(attn_sz, config.n_embd, bias=False)
+        self.output = Linear(attn_sz, config.n_embd, bias=False)
 
         self.key.scale_init = 0
         self.receptance.scale_init = 0
@@ -117,34 +72,34 @@ class RWKV_TimeMix(nn.Module):
         v = self.value(xv)
         r = self.receptance(xr)
 
-        rwkv = torch.sigmoid(r) * RUN_CUDA(B, T, C, self.time_decay, self.time_first, k, v)
+        rwkv = flow.sigmoid(r) * RUN_CUDA(B, T, C, self.time_decay, self.time_first, k, v)
         rwkv = self.output(rwkv)
-
+  
         return rwkv
 
 
 class RWKV_ChannelMix(nn.Module):
     def __init__(self, config, layer_id):
-
+      
         super().__init__()
         self.layer_id = layer_id
 
         self.time_shift = nn.ZeroPad2d((0, 0, 1, -1))
 
-        with torch.no_grad(): # fancy init of time_mix
+        with flow.no_grad(): # fancy init of time_mix
             ratio_1_to_almost0 = (1.0 - (layer_id / config.n_layer)) # 1 to ~0
 
-            x = torch.ones(1, 1, config.n_embd)
+            x = flow.ones(1, 1, config.n_embd)
             for i in range(config.n_embd):
                 x[0, 0, i] = i / config.n_embd
 
-            self.time_mix_k = nn.Parameter(torch.pow(x, ratio_1_to_almost0))
-            self.time_mix_r = nn.Parameter(torch.pow(x, ratio_1_to_almost0))
+            self.time_mix_k = nn.Parameter(flow.pow(x, ratio_1_to_almost0))
+            self.time_mix_r = nn.Parameter(flow.pow(x, ratio_1_to_almost0))
 
         hidden_sz = 4 * config.n_embd
-        self.key = nn.Linear(config.n_embd, hidden_sz, bias=False)
-        self.receptance = nn.Linear(config.n_embd, config.n_embd, bias=False)
-        self.value = nn.Linear(hidden_sz, config.n_embd, bias=False)
+        self.key = Linear(config.n_embd, hidden_sz, bias=False)
+        self.receptance = Linear(config.n_embd, config.n_embd, bias=False)
+        self.value = Linear(hidden_sz, config.n_embd, bias=False)
 
         self.value.scale_init = 0
         self.receptance.scale_init = 0
@@ -155,11 +110,16 @@ class RWKV_ChannelMix(nn.Module):
         xr = x * self.time_mix_r + xx * (1 - self.time_mix_r)
 
         k = self.key(xk)
-        k = torch.square(torch.relu(k))
+        k = flow.square(flow.relu(k))
         kv = self.value(k)
 
-        rkv = torch.sigmoid(self.receptance(xr)) * kv
+        rkv = flow.sigmoid(self.receptance(xr)) * kv
         return rkv
+
+########################################################################################################
+# The GPT Model with our blocks
+########################################################################################################
+
 
 class GPTConfig:
     def __init__(self, vocab_size, ctx_len, **kwargs):
@@ -175,11 +135,11 @@ class Block(nn.Module):
         self.config = config
         self.layer_id = layer_id
 
-        self.ln1 = nn.LayerNorm(config.n_embd)
-        self.ln2 = nn.LayerNorm(config.n_embd)
+        self.ln1 = LayerNorm(config.n_embd)
+        self.ln2 = LayerNorm(config.n_embd)
 
         if self.layer_id == 0:
-            self.ln0 = nn.LayerNorm(config.n_embd)
+            self.ln0 = LayerNorm(config.n_embd)
 
         if self.layer_id == 0 and self.config.model_type == 'RWKV-ffnPre':
             self.ffnPre = RWKV_ChannelMix(config, layer_id+1000)
@@ -198,29 +158,29 @@ class Block(nn.Module):
         x = x + self.ffn(self.ln2(x))
         return x
 
+
 class GPT(nn.Module):
     def __init__(self, config):
-        pdb.set_trace()
 
         super().__init__()
         self.step = 0
         self.config = config
 
-        self.emb = nn.Embedding(config.vocab_size, config.n_embd)
+        self.emb = VocabEmbedding(config.vocab_size, config.n_embd)
 
         self.blocks = nn.Sequential(*[Block(config, i)
                                     for i in range(config.n_layer)])
 
-        self.ln_out = nn.LayerNorm(config.n_embd)
-        self.head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
+        self.ln_out = LayerNorm(config.n_embd)
+        self.head = Linear(config.n_embd, config.vocab_size, bias=False)
 
         if RWKV_HEAD_QK_DIM > 0:
-            self.head_q = nn.Linear(config.n_embd, RWKV_HEAD_QK_DIM, bias=False)
+            self.head_q = Linear(config.n_embd, RWKV_HEAD_QK_DIM, bias=False)
             self.head_q.scale_init = 0
-            self.head_k = nn.Linear(config.n_embd, RWKV_HEAD_QK_DIM, bias=False)
+            self.head_k = Linear(config.n_embd, RWKV_HEAD_QK_DIM, bias=False)
             self.head_k.scale_init = 0.1
-            self.register_buffer("copy_mask", torch.tril(
-                torch.ones(config.ctx_len, config.ctx_len)))
+            self.register_buffer("copy_mask", flow.tril(
+                flow.ones(config.ctx_len, config.ctx_len)))
 
         self.ctx_len = config.ctx_len
 
@@ -258,7 +218,7 @@ class GPT(nn.Module):
                         for pn in sorted(list(no_decay))], "weight_decay": 0.0},
         ]
 
-        optimizer = FusedAdam(optim_groups, lr=train_config.learning_rate, betas=train_config.betas, eps=train_config.eps, bias_correction=True, adam_w_mode=False, weight_decay=0, amsgrad=False)
+        optimizer = Fflow.optim.Optimizer
 
         return optimizer
 
@@ -285,6 +245,6 @@ class GPT(nn.Module):
 
         loss = None
         if targets is not None:
-            loss = F.cross_entropy(x.view(-1, x.size(-1)), targets.to(x.device).view(-1))
+            loss = F.ParallelCrossEntropyLoss(x.view(-1, x.size(-1)), targets.to(x.device).view(-1))
 
-        return x, loss 
+        return x, loss
